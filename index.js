8f992a6acfdb3c24658b92cab6181ad7989b6ed1
Winnie <wrm956@gmail.com>
2022-05-18 11:52:22 -0400

    commit tree 196886b452549a657cf81b4fe1297a97aa2d52dd

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "luna"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "otto"
+}
